b7326ca0acf23bb32d1fb405f68574d549cf11b6
Michal Krolikowski <michal.krolikowski1@gmail.com>
2019-05-08 21:51:07 +0200

    Simple map Simple buttons

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string SceneName;
+
+    public void Load() => SceneManager.LoadScene(SceneName);
+}
