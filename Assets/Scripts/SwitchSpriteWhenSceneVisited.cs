de2222cf823a9f5103d7fd44ab6bfbfd5ec356d5
Michal Krolikowski <michal.krolikowski1@gmail.com>
2019-05-19 15:17:22 +0200

    Map markers switch graphic when visited

---
+++ Assets/Scripts/SwitchSpriteWhenSceneVisited.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class SwitchSpriteWhenSceneVisited : MonoBehaviour
+{
+    public string sceneName;
+    public Sprite sprite;
+
+    [Inject]
+    public ExplorationState ExplorationState { set; private get; }
+
+    private void OnEnable()
+    {
+        if (ExplorationState.HasVisitedScene(sceneName))
+            gameObject.GetComponent<Image>().sprite = sprite;
+    }
+}
